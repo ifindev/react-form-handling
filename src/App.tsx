b14ff68c3c580a5b4231bc8c3b23b10eeb618832
Muhammad Arifin <muhammad.arifin@nri.co.id>
2023-07-11 11:12:40 +0700

    feat: refactor login form with react-hook-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,69 @@
-import React, { useRef } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
 import FormInput from "./components/FormInput";
 
-function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
+const LoginFormSchema = z.object({
+  username: z.string().nonempty({ message: "Username is required" }).min(6, {
+    message:
+      "Invalid username! Valid username should contain minimum 6 characters",
+  }),
+  password: z.string().nonempty({ message: "Password is required" }).min(6, {
+    message:
+      "Invalid password! Valid password should contain minimum 6 characters",
+  }),
+});
 
-  const handleFocusInput = () => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  };
+type LoginForm = z.infer<typeof LoginFormSchema>;
+type LoginFormData = LoginForm & FieldValues;
+
+function App() {
+  // #REGION React Hook Form
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, defaultValues },
+  } = useForm<LoginFormData>({
+    mode: "onBlur",
+    resolver: zodResolver(LoginFormSchema),
+  });
+
+  const onSubmit: SubmitHandler<LoginForm> = (data) =>
+    console.log("data", data);
+
+  // #ENDREGION
 
   return (
-    <div className="p-4 bg w-screen h-screen">
-      <FormInput
-        type="text"
-        label="Username"
-        ref={inputRef}
-        className="w-[300px]"
-      />
-      <button
-        className="bg-blue-500 text-white px-6 py-2 rounded mt-2"
-        onClick={handleFocusInput}
-      >
-        Edit
-      </button>
+    <div className="p-4 bg w-[300px]">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <FormInput<LoginFormData>
+          type="text"
+          name="username"
+          label="Username"
+          placeholder="Username"
+          error={errors.username?.message}
+          register={register}
+          defaultValue={defaultValues?.username}
+        />
+        <FormInput<LoginFormData>
+          className="mt-2"
+          type="password"
+          name="password"
+          label="Password"
+          placeholder="Password"
+          error={errors.password?.message}
+          register={register}
+          defaultValue={defaultValues?.password}
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-6 py-2 rounded mt-4"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
