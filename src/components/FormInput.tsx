2e7e9796df1f2aae5ab541670f3f3bdac1dce07a
Muhammad Arifin <muhammad.arifin@nri.co.id>
2023-07-11 11:12:16 +0700

    feat: refactor FormInput with react-hook-form

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,44 +1,44 @@
 import clsx from "clsx";
-import React, {
-  DetailedHTMLProps,
-  ForwardedRef,
-  InputHTMLAttributes,
-  forwardRef,
-} from "react";
+import React, { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import { FieldPath, FieldValues, UseFormRegister } from "react-hook-form";
 
-type Props = DetailedHTMLProps<
-  InputHTMLAttributes<HTMLInputElement>,
-  HTMLInputElement
+type Props<TFieldValues extends FieldValues> = Omit<
+  DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
+  "name"
 > & {
+  name: FieldPath<TFieldValues>;
   label?: string;
   className?: string;
   error?: string;
+  register: UseFormRegister<TFieldValues>;
 };
 
-function FormInput(
-  { label, className, error, ...rest }: Props,
-  ref: ForwardedRef<HTMLInputElement>
-) {
+function FormInput<TFieldValues extends FieldValues>({
+  label,
+  className,
+  error,
+  register,
+  name,
+  ...rest
+}: Props<TFieldValues>) {
   return (
     <div className={clsx("flex flex-col", className)}>
-      <label htmlFor={label} className="text-gray-900 text-base ">
+      <label htmlFor={label} className="text-gray-900 text-base font-bold mb-1">
         {label}
       </label>
       <input
-        ref={ref}
         {...rest}
-        name={label}
-        type="text"
         className={clsx(
           "p-2 w-full",
           "border border-gray-400 rounded text-base text-gray-700",
           "focus:outline-blue-500 focus:border-none",
           error && "border-red-500 focus:outline-red-500"
         )}
+        {...register(name)}
       />
       <span className="text-xs mt-1 text-red-500">{error}</span>
     </div>
   );
 }
 
-export default forwardRef(FormInput);
+export default FormInput;
