d0612be1f72ba0036853d256e005b885f988d690
Muhammad Arifin <muhammad.arifin@nri.co.id>
2023-07-11 12:01:17 +0700

    feat: refactor form handler with useLoginForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,25 @@
-import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import { z } from "zod";
 import FormInput from "./components/FormInput";
-
-const LoginFormSchema = z.object({
-  username: z.string().nonempty({ message: "Username is required" }).min(6, {
-    message:
-      "Invalid username! Valid username should contain minimum 6 characters",
-  }),
-  password: z.string().nonempty({ message: "Password is required" }).min(6, {
-    message:
-      "Invalid password! Valid password should contain minimum 6 characters",
-  }),
-});
-
-type LoginForm = z.infer<typeof LoginFormSchema>;
-type LoginFormData = LoginForm & FieldValues;
+import useLoginForm, { LoginFormData } from "./hooks/useLoginForm.hook";
 
 function App() {
   // #REGION React Hook Form
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, defaultValues },
-  } = useForm<LoginFormData>({
-    mode: "onBlur",
-    resolver: zodResolver(LoginFormSchema),
-  });
-
-  const onSubmit: SubmitHandler<LoginForm> = (data) =>
-    console.log("data", data);
+  const loginForm = useLoginForm({ validationMode: "onBlur" });
 
   // #ENDREGION
 
   return (
     <div className="p-4 bg w-[300px]">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={loginForm.handleSubmit(loginForm.onSubmit)}>
         <FormInput<LoginFormData>
           type="text"
           name="username"
           label="Username"
           placeholder="Username"
-          error={errors.username?.message}
-          register={register}
-          defaultValue={defaultValues?.username}
+          error={loginForm.errors.username?.message}
+          register={loginForm.register}
+          defaultValue={loginForm.defaultValues?.username}
         />
         <FormInput<LoginFormData>
           className="mt-2"
@@ -53,9 +27,9 @@ function App() {
           name="password"
           label="Password"
           placeholder="Password"
-          error={errors.password?.message}
-          register={register}
-          defaultValue={defaultValues?.password}
+          error={loginForm.errors.password?.message}
+          register={loginForm.register}
+          defaultValue={loginForm.defaultValues?.password}
         />
         <button
           type="submit"
