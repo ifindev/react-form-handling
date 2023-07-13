b110fabde6871996006f4b431584b348f357d155
ifindev <arifin.muhammad2610@Gmail.com>
2023-07-13 15:43:41 +0700

    feat: update generic form type to check  nested zod schema

--- src/types/form.type.ts
+++ src/types/form.type.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { CheckNestedObject, Equals, MapNestedObjects } from "./common.type";
 
 /**
  * Generic type for errors returned from zod safe parsing the form schema
@@ -16,18 +17,19 @@ export type FormSchema<TZodSchema extends z.ZodTypeAny> = z.infer<TZodSchema>;
 /**
  * Generic type that accepts a valid zod schema and returns
  * mapped type for form input values where all form fields have string type
+ * including all schema keys with nested object.
  */
-export type FormFieldValues<TZodSchema extends z.ZodTypeAny> = {
-  [K in keyof FormSchema<TZodSchema>]: string;
-};
+export type FormFieldValues<TZodSchema extends z.ZodTypeAny> = MapNestedObjects<
+  FormSchema<TZodSchema>,
+  string
+>;
 
 /**
  * Generic type that accepts a valid zod schema and returns a
  * mapped boolean typescript object type from the schema keys
  */
-export type FormTouchedState<TZodSchema extends z.ZodTypeAny> = {
-  [K in keyof FormSchema<TZodSchema>]: boolean;
-};
+export type FormTouchedState<TZodSchema extends z.ZodTypeAny> =
+  MapNestedObjects<FormSchema<TZodSchema>, boolean>;
 
 /**
  * Generic type for errors returned from zod safe parsing the form schema
@@ -35,3 +37,11 @@ export type FormTouchedState<TZodSchema extends z.ZodTypeAny> = {
 export type FormErrorState<TZodSchema extends z.ZodTypeAny> = FieldErrors<
   FormSchema<TZodSchema>
 >;
+
+/** Generic type to check whether a given zod schema has nested fields or not */
+export type NonNestedZodSchema<TZodSchema extends z.ZodTypeAny> = Equals<
+  CheckNestedObject<FormSchema<TZodSchema>>,
+  FormSchema<TZodSchema>
+> extends true
+  ? TZodSchema
+  : never;
