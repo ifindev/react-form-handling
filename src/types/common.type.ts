4407f03d88c104cde03b5fad89e775c2b287177e
ifindev <arifin.muhammad2610@Gmail.com>
2023-07-13 15:42:59 +0700

    feat: add common types to handle object type

---
+++ src/types/common.type.ts
@@ -0,0 +1,91 @@
+/**
+ * Generic type to transform deep nested object type
+ * so that each keys in the object will have a new type
+ * of TNew
+ *
+ * example :
+ *
+ * type Information = {
+ *   name: string;
+ *   address: {
+ *     street: string
+ *     zipCode: number
+ *   }
+ * }
+ *
+ * If we want to convert all fields in the Information type
+ * to have a boolean type, then we can do this
+ *
+ * type NewInformation = MapNestedObjects<Information, boolean>
+ *
+ * Which is the same as
+ *
+ * type NewInformation = {
+ *   name: boolean;
+ *   address: {
+ *     street: boolean
+ *     zipCode: boolean
+ *   }
+ * }
+ *
+ *
+ */
+export type MapNestedObjects<T extends object, TNew> = {
+  [K in keyof T]: T[K] extends object ? MapNestedObjects<T[K], TNew> : TNew;
+};
+
+/**
+ * Generic type that accepts an object type
+ * and will return the original object type if all
+ * object keys are not of object type
+ *
+ * Example :
+ *
+ * type ValidType = {
+ *   one: string;
+ *   two: string;
+ * }
+ *
+ * CheckNestedObject<Valid> will return
+ * type ValidType = {
+ *   one: string;
+ *   two: string;
+ * }
+ *
+ * Whereas
+ *
+ * type InValidType = {
+ *   one: string;
+ *   nested: {
+ *     a: number;
+ *     b: string;
+ *   }
+ * }
+ *
+ * CheckNestedObject<InValidType> will return
+ *
+ * type InValidType = {
+ *   one: never;
+ *   nested: {
+ *     a: never;
+ *     b: never;
+ *   }
+ * }
+ */
+export type CheckNestedObject<T extends object> = {
+  [K in keyof T]: T[K] extends object ? never : T[K];
+};
+
+/**
+ * Generic type to check whether two given types are stricly equal.
+ * In this type expression, the type variable T is not explicitly
+ * defined or constrained outside the function type.
+ * Instead, it's inferred based on the context in which Equals<X, Y> is used
+ *
+ * Source: https://github.com/Microsoft/TypeScript/issues/27024#issuecomment-421529650
+ */
+export type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <
+  T
+>() => T extends Y ? 1 : 2
+  ? true
+  : false;
