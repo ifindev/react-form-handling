bbee4e4956da75598b726303ccb2ab8c78b386a6
ifindev <arifin.muhammad2610@Gmail.com>
2023-07-14 15:04:55 +0700

    feat: add util function to get field error from zod

--- src/utils/form/form.util.ts
+++ src/utils/form/form.util.ts
@@ -1,5 +1,9 @@
 import { z } from "zod";
-import { FieldErrors, FormSchema } from "../../types/form.type";
+import {
+  FieldErrors,
+  FormSchema,
+  FormTouchedState,
+} from "../../types/form.type";
 
 /**
  * Validates values against a Zod schema and returns any validation errors.
@@ -11,7 +15,7 @@ import { FieldErrors, FormSchema } from "../../types/form.type";
  *
  * @returns {FieldErrors<FormSchema<TZodSchema>>}
  *   - An object containing the field errors,
- *   where each field is a key and the value is either a single string or an array of strings.
+ *   where each field is a key and the value is an array of strings.
  *   - If there are no validation errors, an empty object is returned.
  */
 export function zodValidate<TZodSchema extends z.ZodTypeAny>(
@@ -27,4 +31,40 @@ export function zodValidate<TZodSchema extends z.ZodTypeAny>(
   return result.error.flatten().fieldErrors as FieldErrors<
     FormSchema<TZodSchema>
   >;
+}
+
+/**
+ * Get the error message for a specific form field.
+ *
+ * @template TZodSchema - The Zod schema type for the form.
+ * @param {z.ZodType<any>} schema - The Zod schema for the form.
+ * @param {FormSchema} values - The form values.
+ * @param {FormTouchedState} touchedState - The touched state of form fields.
+ * @param {keyof FormSchema} field - The field name to retrieve the error for.
+ * @returns {string | undefined} - The error message for the field, or undefined if no error or field is not touched.
+ */
+export function getFieldError<
+  TZodSchema extends z.ZodTypeAny,
+  TField extends keyof FormSchema<TZodSchema>
+>(
+  schema: TZodSchema,
+  values: FormSchema<TZodSchema>,
+  touchedState: FormTouchedState<TZodSchema>,
+  field: TField
+): string | undefined {
+  const errors = zodValidate(schema, values);
+
+  if (
+    errors !== undefined &&
+    Object.keys(errors).length !== 0 &&
+    touchedState[field]
+  ) {
+    const fieldError = errors[field];
+
+    if (fieldError && fieldError.length > 0) {
+      return fieldError[0];
+    }
+  }
+
+  return undefined;
 }
